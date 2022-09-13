b99744cbaba45e7b813d9a8244747c2789a6535b
jabo17 <jannick.borowitz@gmail.com>
2022-09-13 08:40:30 +0200

    format

--- wmis/lib/mis/kernel/fast_set.h
+++ wmis/lib/mis/kernel/fast_set.h
@@ -9,35 +9,29 @@
 #include <vector>
 
 class fast_set {
+    std::vector<int> used;
+    int uid;
 
-	std::vector<int> used;
-	int uid;
+   public:
+    fast_set(int const n) : used(n, 0), uid(1) {}
 
-public:
-	fast_set(int const n) : used(n, 0), uid(1)
-    { }
+    void clear() {
+        uid++;
+        if (uid < 0) {
+            for (unsigned int i = 0; i < used.size(); i++) used[i] = 0;
+            uid = 1;
+        }
+    }
 
-	void clear() {
-		uid++;
-		if (uid < 0) {
-			for (unsigned int i = 0; i < used.size(); i++) used[i] = 0;
-			uid = 1;
-		}
-	}
+    bool add(int i) {
+        bool const res(used[i] != uid);
+        used[i] = uid;
+        return res;
+    }
 
-	bool add(int i) {
-		bool const res(used[i] != uid);
-		used[i] = uid;
-		return res;
-	}
+    void remove(int i) { used[i] = uid - 1; }
 
-	void remove(int i) {
-		used[i] = uid - 1;
-	}
-
-	bool get(int i) const {
-		return (used[i] == uid);
-	}
+    bool get(int i) const { return (used[i] == uid); }
 };
 
-#endif // FAST_SET_H
+#endif  // FAST_SET_H
